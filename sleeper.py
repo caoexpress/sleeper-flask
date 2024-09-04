ce8eec3c6b170aefc8f2fb184aaf91205b1bf194
Emeka <caoexpress@gmail.com>
2024-09-04 19:56:29 -0400

    Updated sleeper.py with necessary fixes for deployment

--- sleeper.py
+++ sleeper.py
@@ -60,5 +60,6 @@ def get_all_nfl_players():
     url = "https://api.sleeper.app/v1/players/nfl"
     return fetch_data(url)
 
-if __name__ == '__main__':
+if __name__ == "sleeper":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
